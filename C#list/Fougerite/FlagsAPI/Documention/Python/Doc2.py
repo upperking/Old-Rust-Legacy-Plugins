98cf692113351a6859fd292d6295b9da56194abf
ice cold <wargamingnl7@gmail.com>
2018-05-10 18:43:17 +0200

    Add files via upload

---
+++ C#list/Fougerite/FlagsAPI/Documention/Python/Doc2.py
@@ -0,0 +1,13 @@
+if Player.Admin or self.Hasflag(Player, "yourflagnamehere")
+
+if self.Hasflag(Player, "yourflagnamehere")
+
+if Player.Admin or Player.Moderator or self.Hasflag(Player, "yourflagnamehere")
+
+if Player.Moderator or self.Hasflag(Player, "yourflagnamehere")
+
+if Player.Admin and self.Hasflag(Player, "yourflagnamehere")
+
+if Player.Admin or Player.Moderator and self.Hasflag(Player, "yourflagnamehere")
+
+if Player.Moderator and self.Hasflag(Player, "yourflagnamehere")
