1ac35133d5305d3e26bc8ed497238d67ff5d3962
ice cold <wargamingnl7@gmail.com>
2018-06-11 10:25:33 +0200

    Update Doc1.js

--- C#list/Fougerite/FlagsAPI/Documention/Javascript/Doc1.js
+++ C#list/Fougerite/FlagsAPI/Documention/Javascript/Doc1.js
@@ -1,13 +1,8 @@
- var path = require('path');
- var flagpath = path.basename('\\Save\\FlagsAPI\\PlayerFlagdb.ini');
 
- function On_PluginInit() {
- 	var flag = flagpath;
- }
  function On_Command(Player, cmd, args){
  	if(cmd == "tester")
  	{
- 		if(Hasflag(Player, "testerflag"))
+ 		if(API.HasFlag(Player, "testerflag"))
  		{
  			Player.Notice("Yes");
  		}
@@ -15,12 +10,5 @@
  			Player.Notice("no");
  		}
  	}
- }
- function Hasflag(Player, flag){
- 	var brr = Plugin.CreateIni(flag);
- 	if(brr.ContainsSetting(brr)){
- 		return true;		
- 	}
- 	return false;
  }
 
