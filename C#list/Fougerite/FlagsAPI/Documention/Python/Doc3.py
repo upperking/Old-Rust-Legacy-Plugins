b776fb23a6cb1ac90ae85aaefd3ebe77cc20dab3
ice cold <wargamingnl7@gmail.com>
2018-06-11 10:24:32 +0200

    Delete Doc3.py

--- C#list/Fougerite/FlagsAPI/Documention/Python/Doc3.py
+++
@@ -1,5 +0,0 @@
- def Hasflag(self, Player, str(flag)):
-        brr = Plugin.CreateIni(path)
-        if brr.ContainsSetting(Player.SteamID, str(flag)):
-            return True
-        return False
