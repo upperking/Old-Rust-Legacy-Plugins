bdb70e4349b2a7c8bdeb4c5eab711eb2529a5dd0
ice cold <wargamingnl7@gmail.com>
2018-06-11 10:24:17 +0200

    Update Doc2.py

--- C#list/Fougerite/FlagsAPI/Documention/Python/Doc2.py
+++ C#list/Fougerite/FlagsAPI/Documention/Python/Doc2.py
@@ -1,13 +1,6 @@
-if Player.Admin or self.Hasflag(Player, "yourflagnamehere")
-
-if self.Hasflag(Player, "yourflagnamehere")
-
-if Player.Admin or Player.Moderator or self.Hasflag(Player, "yourflagnamehere")
-
-if Player.Moderator or self.Hasflag(Player, "yourflagnamehere")
-
-if Player.Admin and self.Hasflag(Player, "yourflagnamehere")
-
-if Player.Admin or Player.Moderator and self.Hasflag(Player, "yourflagnamehere")
-
-if Player.Moderator and self.Hasflag(Player, "yourflagnamehere")
+# here some examples
+API.HasFlag(Player, "flagname"):
+API.RemoveFlag(Player, "flagname")
+API.RegisterFlag("flagname")
+API.RemoveFlag("flagname")
+API.GetFlag(Player, "flagname")
