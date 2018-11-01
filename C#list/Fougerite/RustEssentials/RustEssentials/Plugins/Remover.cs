dec824b0b3c48b9e463fac7fce59216334a939f3
ice cold <wargamingnl7@gmail.com>
2018-11-01 20:24:37 +0100

    Update Remover.cs

--- C#list/Fougerite/RustEssentials/RustEssentials/Plugins/Remover.cs
+++ C#list/Fougerite/RustEssentials/RustEssentials/Plugins/Remover.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,48 @@ namespace RustEssentials.Plugins
 {
     public class Remover
     {
+        public static void Start()
+        {
+
+        }
+        public static Dictionary<string, string> entitylist = new Dictionary<string, string>()
+        {
+            {"WoodFoundation", "Wood Foundation" },
+            {"WoodDoorFrame", "Wood Doorway" },
+            {"WoodDoor", "Wood Door" },
+            {"WoodPillar", "Wood Pillar" },
+            {"WoodWall", "Wood Wall" },
+            {"WoodCeiling", "Wood Ceiling" },
+            {"WoodWindowFrame", "Wood Window" },
+            {"WoodStairs", "Wood Stairs" },
+            {"WoodRamp", "Wood Ramp" },
+            {"WoodSpikeWall", "Spike Wall" },
+            {"LargeWoodSpikeWall", "Large Spike Wall" },
+            {"WoodBox", "Wood Storage Box" },
+            {"WoodBoxLarge", "large Wood Storage" },
+            {"WoodGate", "Wood Gate" },
+            {"WoodGateway", "Wood Gateway" },
+            {"WoodenDoor", "Wood Door" },
+            {"Wood_Shelter", "Wood Shelter" },
+            {"MetalWall", "Metal Wall" },
+            {"MetalCeiling",  "Metal Ceiling"},
+            {"MetalDoorFrame", "Metal Doorway" },
+            {"MetalPillar", "Metal Pillar" },
+            {"MetalFoundation", "Metal Foundation" },
+            {"MetalStairs", "Metal Stairs" },
+            {"MetalRamp", "Metal Ramp" },
+            {"MetalWindowFrame", "Metal Window" },
+            {"MetalDoor", "Metal Door" },
+            {"MetalBarsWindow", "Metal Window bars" },
+            {"SmallStash", "Small Stash" },
+            {"Campfire", "Camp Fire" },
+            {"Furnace", "Furnace" },
+            {"Workbench", "Workbench" },
+            {"Wood Barricade", "Wood Barricade" },
+            {"RepairBench", "Repair Bench" },
+            {"SleepingBagA", "Sleeping Bag" },
+            {"SingleBed", "Bed" }
+        };
+
     }
 }
